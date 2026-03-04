b0209a1145526b957641730a2b71daf52efa73bc
LE BON COIN <saidguerda04@gmail.com>
2026-03-04 15:09:00 -0500

    j'ai créer la class voiture et ses attributs

--- parcvoiture.py
+++ parcvoiture.py
@@ -4,5 +4,10 @@ class voiture:
         self.marque = marque
         self.couleur = couleur
     def afficher_informations(self):
-        print(f"la matricule de la voiture: {self.matricule},et ca marque: {self.marque},et couleur: {self.couleur}"
-              )
+        print(f"la matricule de la voiture: {self.matricule},et ca marque: {self.marque},et couleur: {self.couleur}")
+class Parc:
+    def __init__(self,id,adresse,capacite):
+        self.id = id
+        self.adresse = adresse
+        self.capacite = capacite
+        self.liste_voitures = []
